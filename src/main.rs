f41a4e8067a409bcb6a024e8bf9d5b99c1e46374
countzero <countzeronm@gmail.com>
2024-09-05 22:07:52 +0900

    3次元ボックス p85

--- src/main.rs
+++ src/main.rs
@@ -23,10 +23,10 @@ type Vector3 = cgmath::Vector3<f32>;
 #[allow(dead_code)]
 type Matrix4 = cgmath::Matrix4<f32>;
 
-const WINDOW_WIDTH: u32 = 640;
+const WINDOW_WIDTH: u32 = 900;
 const WINDOW_HEIGHT: u32 = 480;
 const FLOAT_NUM: usize = 3;
-const VERTEX_NUM: usize = 3;
+const VERTEX_NUM: usize = 36;
 const BUF_LEN: usize = FLOAT_NUM * VERTEX_NUM;
 
 fn main() {
@@ -56,9 +56,59 @@ fn main() {
     // set buffer
     #[rustfmt::skip]
     let buffer_array: [f32; BUF_LEN] = [
-        -1.0, -1.0, 0.0,
-        1.0, -1.0, 0.0,
+        // 1
+        0.0, 0.0, 0.0,
         0.0, 1.0, 0.0,
+        1.0, 1.0, 0.0,
+
+        0.0, 0.0, 0.0,
+        1.0, 1.0, 0.0,
+        1.0, 0.0, 0.0,
+
+        // 2
+        0.0, 0.0, 1.0,
+        0.0, 0.0, 0.0,
+        1.0, 0.0, 0.0,
+
+        0.0, 0.0, 1.0,
+        1.0, 0.0, 0.0,
+        1.0, 0.0, 1.0,
+
+        // 3
+        0.0, 1.0, 1.0,
+        0.0, 0.0, 1.0,
+        1.0, 0.0, 1.0,
+
+        0.0, 1.0, 1.0,
+        1.0, 0.0, 1.0,
+        1.0, 1.0, 1.0,
+
+        // 4
+        0.0, 1.0, 0.0,
+        0.0, 1.0, 1.0,
+        1.0, 1.0, 1.0,
+
+        0.0, 1.0, 0.0,
+        1.0, 1.0, 1.0,
+        1.0, 1.0, 0.0,
+
+        // 5
+        1.0, 0.0, 1.0,
+        1.0, 0.0, 0.0,
+        1.0, 1.0, 0.0,
+
+        1.0, 0.0, 1.0,
+        1.0, 1.0, 0.0,
+        1.0, 1.0, 1.0,
+
+        // 6
+        0.0, 1.0, 1.0,
+        0.0, 1.0, 0.0,
+        0.0, 0.0, 0.0,
+
+        0.0, 1.0, 1.0,
+        0.0, 0.0, 0.0,
+        0.0, 0.0, 1.0,
     ];
 
     let vertex = Vertex::new(
@@ -75,12 +125,19 @@ fn main() {
     let mut imgui_context = imgui::Context::create();
     imgui_context.set_ini_filename(None);
 
-
     // init imgui sdl2
     let mut imgui_sdl2_context = imgui_sdl2::ImguiSdl2::new(&mut imgui_context, &window);
     let renderer = imgui_opengl_renderer::Renderer::new(&mut imgui_context, |s| {
         video_subsystem.gl_get_proc_address(s) as _
     });
+
+    let mut depth_test: bool = true;
+    let mut blend: bool = true;
+    let mut wireframe: bool = true;
+    let mut culling: bool = true;
+    let mut camera_x: f32 = 5.0f32;
+    let mut camera_y: f32 = 5.0f32;
+    let mut camera_z: f32 = 5.0f32;
 
     let mut event_pump = sdl_context.event_pump().unwrap();
     'running: loop {
@@ -101,29 +158,54 @@ fn main() {
         }
 
         unsafe {
+            if depth_test {
+                gl::Enable(gl::DEPTH_TEST);
+            } else {
+                gl::Disable(gl::DEPTH_TEST);
+            }
+            
+            if blend {
+                gl::Enable(gl::BLEND);
+                gl::BlendFunc(gl::SRC_ALPHA, gl::ONE_MINUS_SRC_ALPHA);
+            } else {
+                gl::Disable(gl::BLEND);
+            }
+
+            if wireframe {
+                gl::PolygonMode(gl::FRONT_AND_BACK, gl::LINE);
+            } else {
+                gl::PolygonMode(gl::FRONT_AND_BACK, gl::FILL);
+            }
+
+            if culling {
+                gl::Enable(gl::CULL_FACE);
+            } else {
+                gl::Disable(gl::CULL_FACE);
+            }
+
             gl::Viewport(0, 0, WINDOW_WIDTH as i32, WINDOW_HEIGHT as i32);
 
             // clear screen
             gl::ClearColor(1.0, 1.0, 1.0, 1.0);
-            gl::Clear(gl::COLOR_BUFFER_BIT);
+            gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
 
             // init matrice for model, view and projection
             let model_matrix = Matrix4::identity();
             let view_matrix = Matrix4::look_at(
                 Point3 {
-                    x: 0.0,
-                    y: 0.0,
-                    z: 5.0,
+                    x: camera_x,
+                    y: camera_y,
+                    z: camera_z,
                 },
                 Point3 {
-                    x: 0.0,
-                    y: 0.0,
-                    z: 0.0,
+                    x: 0.5,
+                    y: 0.5,
+                    z: 0.5,
                 },
                 Vector3 {
                     x: 0.0,
-                    y: 1.0,
-                    z: 0.0,
+                    y: 0.0,
+                    z: 1.0,
                 },
             );
             let projection_matrix: Matrix4 = perspective(
@@ -149,7 +231,7 @@ fn main() {
 
             let ui = imgui_context.frame();
                 imgui::Window::new(im_str!("Infomation"))
-                    .size([300.0, 240.0], imgui::Condition::FirstUseEver)
+                    .size([300.0, 440.0], imgui::Condition::FirstUseEver)
                     .build(&ui, || {
 
                         ui.text(im_str!("OpenGL Test App ver 1.0"));
@@ -170,6 +252,25 @@ fn main() {
                                 "Mouse Position: ({:.1}, {:.1})",
                                 mouse_pos[0],mouse_pos[1]
                         ));
+
+                        ui.separator();
+
+                        ui.checkbox(im_str!("Depth Test"), &mut depth_test);
+                        ui.checkbox(im_str!("Blend"), &mut blend);
+                        ui.checkbox(im_str!("Wireframe"), &mut wireframe);
+                        ui.checkbox(im_str!("Culling"), &mut culling);
+
+                        ui.separator();
+
+                        # [rustfmt::skip]
+                        imgui::Slider::new(im_str!("Camera X"), -5.0..=5.0)
+                            .build(&ui, &mut camera_x);
+                        # [rustfmt::skip]
+                        imgui::Slider::new(im_str!("Camera Y"), -5.0..=5.0)
+                            .build(&ui, &mut camera_y);
+                        # [rustfmt::skip]
+                        imgui::Slider::new(im_str!("Camera Z"), -5.0..=5.0)
+                            .build(&ui, &mut camera_z);
 
                         imgui::ProgressBar::new(0.3)
                             .size([200.0, 20.0])
